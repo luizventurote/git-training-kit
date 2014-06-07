7c8dfed20c07f9625edff729685eb08b6c8b3998
luizventurote <luiz_17@ymail.com>
2014-06-07 16:44:30 -0300

    Classe de processamento e metodo para adicionar um numero na tela foram adicionados.

---
+++ exemplos/calculadora/src/calculadora/Processador.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+import javax.swing.JTextField;
+
+public class Processador {
+    
+    /**
+     * Adiciona um número na tela.
+     * 
+     * @param tela
+     * @param num Número a ser adicionado.
+     */
+    public static void addNumTela(JTextField tela, int num) {
+        
+        String tela_cache = tela.getText();
+        
+        tela.setText(tela_cache + Integer.toString(num));
+        
+    }
+    
+}
